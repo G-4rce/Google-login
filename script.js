8001d2cc32375fcc62acf2bf3f59aed5ff1754bb
G-4rce <95035615+G-4rce@users.noreply.github.com>
2021-12-01 21:07:13 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+function createAccount(){
+    window.location.href = 'https://accounts.google.com/signup/v2/webcreateaccount?service=mail&continue=https%3A%2F%2Fmail.google.com&hl=en&biz=false&flowName=GlifWebSignIn&flowEntry=SignUp';
+}
+
+function Next_but(){
+    document.getElementById("input2").type = "text";
+    SubForm();
+    window.location.href = 'Jy is cool.html';
+}
+
